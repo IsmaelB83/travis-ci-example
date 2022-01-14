ce803eaf0fff7addb754d219221981d9bad351a6
Ismael <trama1983@hotmail.com>
2022-01-14 06:56:37 +0100

    Using variables in travis-ci

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log('Containers rule! Updated to v1.0');
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
